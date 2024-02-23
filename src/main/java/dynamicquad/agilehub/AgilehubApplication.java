12200555538d4c5cde7473cdaeeb92e8d4fc8d6c
enble <enble_777@naver.com>
2024-02-23 23:09:14 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/dynamicquad/agilehub/AgilehubApplication.java
@@ -0,0 +1,13 @@
+package dynamicquad.agilehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dynamicquad/agilehub/AgilehubApplicationTests.java
@@ -0,0 +1,13 @@
+package dynamicquad.agilehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
