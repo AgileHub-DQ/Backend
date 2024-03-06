28dab49a4855aedf3e690d65d55f4ac60a65d73c
Enble <enble_777@naver.com>
2024-03-06 14:37:35 +0900

    feat: 실패시의 응답 생성 메서드 구현
    CommonResponse.onFailure()는 ExceptionAdvice에서의 실패 응답 핸들링을 위해 사용되는 메서드입니다.

--- src/main/java/dynamicquad/agilehub/global/header/CommonResponse.java
+++ src/main/java/dynamicquad/agilehub/global/header/CommonResponse.java
@@ -12,6 +12,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 public class CommonResponse<T> {
+
     @JsonProperty("isSuccess")
     private final boolean isSuccess;
 
@@ -22,12 +23,18 @@ public class CommonResponse<T> {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private final T result;
 
+    // 성공시 응답 생성
     public static <T> CommonResponse<T> onSuccess(T result) {
         return new CommonResponse<>(true, SuccessStatus.OK.getCode(), SuccessStatus.OK.getMessage(), result);
     }
 
     public static <T> CommonResponse<T> of(BaseStatus status, T result) {
-        return new CommonResponse<>(
-                status.getReason().isSuccess(), status.getReason().getCode(), status.getReason().getMessage(), result);
+        return new CommonResponse<>(true, status.getReason().getCode(), status.getReason().getMessage(), result);
     }
+
+    // 실패시 응답 생성
+    public static <T> CommonResponse<T> onFailure(BaseStatus status, T data) {
+        return new CommonResponse<>(false, status.getReason().getCode(), status.getReason().getMessage(), data);
+    }
+
 }
