f1592bf33139cd91eb26e8906fdebe75572db1b5
minsang-alt <mazinger97@naver.com>
2024-03-12 01:56:39 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/dynamicquad/agilehub/global/domain/BaseEntity.java
@@ -0,0 +1,29 @@
+package dynamicquad.agilehub.global.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedBy
+    private LocalDateTime updatedAt;
+
+}
