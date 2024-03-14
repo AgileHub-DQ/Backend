f309db381273695764652ae52f54e5a75a8870fc
minsang-alt <mazinger97@naver.com>
2024-03-14 09:46:39 +0900

    refactor(SprintStatus): description 제거

--- src/main/java/dynamicquad/agilehub/sprint/domain/SprintStatus.java
+++ src/main/java/dynamicquad/agilehub/sprint/domain/SprintStatus.java
@@ -1,14 +1,7 @@
 package dynamicquad.agilehub.sprint.domain;
 
 public enum SprintStatus {
-
-    PLANNED("진행전"),
-    ACTIVE("진행중"),
-    COMPLETED("완료됨");
-
-    private final String description;
-
-    SprintStatus(String description) {
-        this.description = description;
-    }
+    PLANNED,
+    ACTIVE,
+    COMPLETED
 }
