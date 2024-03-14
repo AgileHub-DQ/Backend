0dc0ed4a33de942284eb5caab147ff69a0450c33
minsang-alt <mazinger97@naver.com>
2024-03-14 09:24:10 +0900

    refactor(BaseEntity): accessLevel protected로 변경

--- src/main/java/dynamicquad/agilehub/global/domain/BaseEntity.java
+++ src/main/java/dynamicquad/agilehub/global/domain/BaseEntity.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDate;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
@@ -13,8 +13,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
