aad6f4d15535efde802fe0e565f0678651b1af4d
enble <enble_777@naver.com>
2024-03-18 00:16:59 +0900

    feat: findByProviderAndDistinctCode 구현

---
+++ src/main/java/dynamicquad/agilehub/member/service/SocialLoginService.java
@@ -0,0 +1,25 @@
+package dynamicquad.agilehub.member.service;
+
+import dynamicquad.agilehub.global.domain.OAuth2Attribute;
+import dynamicquad.agilehub.member.domain.Member;
+import dynamicquad.agilehub.member.domain.SocialLogin;
+import dynamicquad.agilehub.member.repository.SocialLoginRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class SocialLoginService {
+
+    private final SocialLoginRepository socialLoginRepository;
+
+    public Optional<Member> findByProviderAndDistinctCode(String providerId, String distinctCode) {
+        Optional<SocialLogin> socialLogin = socialLoginRepository.findByProviderAndDistinctId(
+                OAuth2Attribute.of(providerId), distinctCode);
+        return socialLogin.map(SocialLogin::getMember);
+    }
+
+}
