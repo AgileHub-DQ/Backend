58e9c3da5f29acc3d12957b6e0609ad63c23535f
enble <enble_777@naver.com>
2024-03-17 23:47:09 +0900

    feat: SocialLogin 엔티티 구현

---
+++ src/main/java/dynamicquad/agilehub/member/domain/SocialLogin.java
@@ -0,0 +1,36 @@
+package dynamicquad.agilehub.member.domain;
+
+import dynamicquad.agilehub.global.domain.BaseEntity;
+import dynamicquad.agilehub.global.domain.OAuth2Attribute;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "social_login")
+public class SocialLogin extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "social_login_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "provider", nullable = false)
+    private OAuth2Attribute provider;
+
+    @Column(name = "distinct_code", nullable = false)
+    private String distinctCode;
+
+}
+
