b41595b4248c10cd6ccab85e6f90c5cefe0f7171
enble <enble_777@naver.com>
2024-03-18 00:00:48 +0900

    feat: registrationId -> OAuth2Attribute 변환 코드 추가

---
+++ src/main/java/dynamicquad/agilehub/global/domain/OAuth2Attribute.java
@@ -0,0 +1,42 @@
+package dynamicquad.agilehub.global.domain;
+
+import dynamicquad.agilehub.global.exception.GeneralException;
+import dynamicquad.agilehub.global.header.status.ErrorStatus;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum OAuth2Attribute {
+    KAKAO("kakao", attributes -> {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+
+        return new OAuth2UserInfo(
+                attributes,
+                attributes.get("id").toString(),
+                profile.get("nickname").toString(),
+                profile.get("profile_image_url").toString()
+        );
+    });
+
+    private final String registrationId;
+    private final Function<Map<String, Object>, OAuth2UserInfo> of;
+
+    public static OAuth2UserInfo getUserInfo(String providerId, Map<String, Object> attributes) {
+        return Arrays.stream(values())
+                .filter(provider -> provider.registrationId.equals(providerId))
+                .findFirst()
+                .orElseThrow(() -> new GeneralException(ErrorStatus.UNSUPPORTED_PROVIDER))
+                .of.apply(attributes);
+    }
+
+    public static OAuth2Attribute of(String registrationId) {
+        return Arrays.stream(values())
+                .filter(provider -> provider.registrationId.equals(registrationId))
+                .findFirst()
+                .orElseThrow(() -> new GeneralException(ErrorStatus.UNSUPPORTED_PROVIDER));
+    }
+
+}
