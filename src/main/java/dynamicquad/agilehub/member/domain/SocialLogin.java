25d55ab47d9d6ddc6b751d4fa70fb3f379b61430
enble <enble_777@naver.com>
2024-03-17 23:56:05 +0900

    refactor: SocialLogin과 Member의 연관관계 추가

--- src/main/java/dynamicquad/agilehub/member/domain/SocialLogin.java
+++ src/main/java/dynamicquad/agilehub/member/domain/SocialLogin.java
@@ -6,9 +6,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -31,6 +34,10 @@ public class SocialLogin extends BaseEntity {
 
     @Column(name = "distinct_code", nullable = false)
     private String distinctCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
 }
 
