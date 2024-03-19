08601f18bc360e4b484b9b66907cf154b92f9a6a
minsang-alt <mazinger97@naver.com>
2024-03-19 21:02:05 +0900

    feat(MemberProjectRepository): 멤버_프로젝트 레포지토리 구현

---
+++ src/main/java/dynamicquad/agilehub/member/domain/MemberRepository.java
@@ -0,0 +1,9 @@
+package dynamicquad.agilehub.member.domain;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+
+}
