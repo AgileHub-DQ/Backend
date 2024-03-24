9cd06c8e07860fae20620e456985b5f0a5944cbc
enble <enble_777@naver.com>
2024-03-24 19:41:06 +0900

    feat: toSocialLogin 메서드 추가

--- src/main/java/dynamicquad/agilehub/member/converter/MemberConverter.java
+++ src/main/java/dynamicquad/agilehub/member/converter/MemberConverter.java
@@ -2,6 +2,7 @@ package dynamicquad.agilehub.member.converter;
 
 import dynamicquad.agilehub.member.domain.Member;
 import dynamicquad.agilehub.member.domain.MemberStatus;
+import dynamicquad.agilehub.member.domain.SocialLogin;
 import dynamicquad.agilehub.member.dto.MemberRequestDto;
 
 public class MemberConverter {
@@ -14,6 +15,14 @@ public class MemberConverter {
                 .name(dto.getName())
                 .profileImageUrl(dto.getProfileImageUrl())
                 .status(MemberStatus.ACTIVE)
+                .build();
+    }
+
+    public static SocialLogin toSocialLogin(Member member, MemberRequestDto.CreateSocialLogin dto) {
+        return SocialLogin.builder()
+                .provider(dto.getProvider())
+                .distinctId(dto.getDistinctId())
+                .member(member)
                 .build();
     }
 
