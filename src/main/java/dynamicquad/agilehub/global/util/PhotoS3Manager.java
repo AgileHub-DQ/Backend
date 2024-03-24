d0e3bf111b7ec767cf94dfa95bbc1788b5c298d1
Min Sang <minseusang@gmail.com>
2024-03-24 21:05:34 +0900

    [#38] s3 image upload 및 삭제 구현 (#39)
    * feat: s3 image upload 및 삭제 구현
    
    * refactor: IllegalException에서 GeneralException으로 수정

---
+++ src/main/java/dynamicquad/agilehub/global/util/PhotoS3Manager.java
@@ -0,0 +1,124 @@
+package dynamicquad.agilehub.global.util;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import dynamicquad.agilehub.global.exception.GeneralException;
+import dynamicquad.agilehub.global.header.status.ErrorStatus;
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PhotoS3Manager implements PhotoManager {
+
+    private static final String SYSTEM_PATH = System.getProperty("user.dir"); // 현재 디렉터리
+
+    @Value("${aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${aws.s3.rootURL}")
+    private String rootURL;
+
+    @Value("${aws.s3.directory}")
+    private String localDirectory;
+
+    private final AmazonS3 amazonS3;
+
+
+    @Override
+    public String upload(MultipartFile multipartFile, String workingDirectory) {
+
+        if (multipartFile == null || multipartFile.isEmpty()) {
+            throw new GeneralException(ErrorStatus.FILE_NOT_EXIST);
+        }
+
+        return uploadPhoto(multipartFile, workingDirectory);
+    }
+
+    private String uploadPhoto(MultipartFile multipartFile, String workingDirectory) {
+        String fileName = createFileName(multipartFile.getOriginalFilename());
+        //임시 디렉터리 생성
+        File tempUploadDirectory = uploadDirectory(getLocalDirectoryPath(workingDirectory));
+        File tempUploadPath = new File(tempUploadDirectory, fileName);
+        File file = uploadFileInLocal(multipartFile, tempUploadPath);
+
+        amazonS3.putObject(new PutObjectRequest(bucket + workingDirectory, fileName, file));
+
+        file.delete();
+
+        return rootURL + "/" + workingDirectory + "/" + fileName;
+
+    }
+
+    private File uploadFileInLocal(MultipartFile multipartFile, File tempUploadPath) {
+        try {
+            multipartFile.transferTo(tempUploadPath);
+        } catch (IOException e) {
+            log.error("파일 변환 실패", e);
+            throw new GeneralException(ErrorStatus.FILE_CONVERT_FAIL);
+        }
+        return tempUploadPath;
+    }
+
+    private File uploadDirectory(String localDirectoryPath) {
+        File directory = new File(localDirectoryPath);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        return directory;
+    }
+
+    private String getLocalDirectoryPath(String workingDirectory) {
+        return SYSTEM_PATH + "/" + localDirectory + workingDirectory;
+    }
+
+
+    private String createFileName(String originalFileName) {
+        String extension = StringUtils.getFilenameExtension(originalFileName); // 확장자
+        if (extension == null) {
+            throw new GeneralException(ErrorStatus.FILE_EXTENSION_NOT_EXIST);
+        }
+        String fileBaseName = UUID.randomUUID().toString().substring(0, 8);
+        validateFileName(fileBaseName);
+        validateExtension(extension);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(fileBaseName).append("_").append(System.currentTimeMillis()).append(".").append(extension);
+        return sb.toString();
+    }
+
+    private void validateFileName(String fileName) {
+        if (StringUtils.hasText(fileName)) {
+            throw new GeneralException(ErrorStatus.FILE_NAME_NOT_EXIST);
+        }
+    }
+
+    private void validateExtension(String extension) {
+        Set<String> IMAGE_EXTENSIONS = Set.of("jpeg", "jpg", "png", "webp", "heic", "heif");
+
+        if (!IMAGE_EXTENSIONS.contains(extension.toLowerCase())) {
+            throw new GeneralException(ErrorStatus.FILE_EXTENSION_NOT_IMAGE);
+        }
+    }
+
+
+    @Override
+    public void delete(String originalImageUrl, String workingDirectory) {
+        if (originalImageUrl.contains(rootURL + "/" + workingDirectory)) {
+            String fileName = originalImageUrl.substring(originalImageUrl.length() + 1);
+            amazonS3.deleteObject(bucket + workingDirectory, fileName);
+            amazonS3.deleteObject(bucket, fileName);
+        }
+    }
+
+}
