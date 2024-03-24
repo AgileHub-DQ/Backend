071dffd1390587f9eebf8ee9fbd939cdc089c824
enble <enble_777@naver.com>
2024-03-24 19:01:00 +0900

    feat: MemberConverter 생성

---
+++ src/main/java/dynamicquad/agilehub/member/converter/MemberConverter.java
@@ -0,0 +1,20 @@
+package dynamicquad.agilehub.member.converter;
+
+import dynamicquad.agilehub.member.domain.Member;
+import dynamicquad.agilehub.member.domain.MemberStatus;
+import dynamicquad.agilehub.member.dto.MemberRequestDto;
+
+public class MemberConverter {
+
+    private MemberConverter() {
+    }
+
+    public static Member toEntity(MemberRequestDto.CreateMember dto) {
+        return Member.builder()
+                .name(dto.getName())
+                .profileImageUrl(dto.getProfileImageUrl())
+                .status(MemberStatus.ACTIVE)
+                .build();
+    }
+
+}
