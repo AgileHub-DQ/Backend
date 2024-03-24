2b17ffde1adf888a6fd8504061b111f1c4394771
Min Sang <minseusang@gmail.com>
2024-03-24 18:17:46 +0900

    [#35] CQRS 패턴 적용와 DTO inner class화 (#37)
    * refactor: xxxrequestDto inner Dto 클래스로 리펙터링
    
    * refactor: swagger 작성 및 put 메서드로 변경
    
    * refactor: ProjectService Command와 Query 서비스 분리

--- src/main/java/dynamicquad/agilehub/project/controller/response/ProjectResponse.java
+++ src/main/java/dynamicquad/agilehub/project/controller/response/ProjectResponse.java
@@ -9,14 +9,14 @@ import lombok.Getter;
 @Builder
 @Getter
 @EqualsAndHashCode
-public class ProjectRes {
+public class ProjectResponse {
     private Long id;
     private String key;
     private String name;
     private LocalDateTime createdAt;
 
-    public static ProjectRes fromEntity(Project project) {
-        return ProjectRes.builder()
+    public static ProjectResponse fromEntity(Project project) {
+        return ProjectResponse.builder()
             .id(project.getId())
             .key(project.getKey())
             .name(project.getName())
