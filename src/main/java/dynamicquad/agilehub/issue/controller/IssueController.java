13be8e3e4d6340583fb21482b8dc5964dff43788
minsang-alt <mazinger97@naver.com>
2024-03-26 00:37:11 +0900

    feat: IssueController 이슈생성부분 구현

--- src/main/java/dynamicquad/agilehub/issue/controller/IssueController.java
+++ src/main/java/dynamicquad/agilehub/issue/controller/IssueController.java
@@ -1,9 +1,15 @@
 package dynamicquad.agilehub.issue.controller;
 
 import dynamicquad.agilehub.global.header.CommonResponse;
+import dynamicquad.agilehub.global.header.status.SuccessStatus;
+import dynamicquad.agilehub.issue.controller.request.IssueRequest.IssueCreateRequest;
+import dynamicquad.agilehub.issue.service.IssueService;
+import jakarta.validation.Valid;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,19 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class IssueController {
 
+    private final IssueService issueService;
 
-    @GetMapping("/api/projects/{key}/boards")
-    public String getProjectBoards(@PathVariable("key") String key) {
-        log.info("getProjectBoards");
-        log.info("key: {}", key);
-        return "project-boards";
+
+    @PostMapping("/api/projects/{key}/issues")
+    public ResponseEntity<?> createProjectIssue(@PathVariable("key") String key, @Valid @ModelAttribute
+    IssueCreateRequest request) {
+        log.info("createProjectIssue key: {}", key);
+        Long issueId = issueService.createIssue(key, request);
+
+        return ResponseEntity.created(URI.create("/api/projects/" + key + "/issues/" + issueId))
+            .body(CommonResponse.of(SuccessStatus.CREATED, issueId));
     }
 
-    @PostMapping("/api/projects/{key}/boards")
-    public CommonResponse<?> createProjectIssue(@PathVariable("key") String key) {
-        log.info("createProjectIssue");
-
-        return null;
-    }
 
 }
---
+++ src/test/java/dynamicquad/agilehub/issue/controller/IssueControllerTest.java
@@ -0,0 +1,70 @@
+package dynamicquad.agilehub.issue.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import dynamicquad.agilehub.issue.controller.request.IssueRequest.IssueCreateRequest;
+import dynamicquad.agilehub.issue.controller.request.IssueType;
+import dynamicquad.agilehub.issue.domain.IssueStatus;
+import dynamicquad.agilehub.issue.service.IssueService;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+@ActiveProfiles("test")
+@WebMvcTest(IssueController.class)
+class IssueControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @MockBean
+    private IssueService issueService;
+
+    @Test
+    void 정상적인_이슈를_생성하면_해당_이슈를_가진_URL을_반환한다() throws Exception {
+
+        //given
+        IssueCreateRequest request = IssueCreateRequest.builder()
+            .title("이슈제목")
+            .type(IssueType.EPIC)
+            .status(IssueStatus.DO)
+            .content("이슈내용")
+            .files(null)
+            .startDate(LocalDate.of(2021, 1, 1))
+            .endDate(LocalDate.of(2021, 1, 2))
+            .assigneeId(1L)
+            .build();
+
+        String key = "project";
+        when(issueService.createIssue(key, request)).thenReturn(1L);
+
+        mockMvc.perform(post("/api/projects/" + key + "/issues")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("title", request.getTitle())
+                .param("type", request.getType().name())
+                .param("status", request.getStatus().name())
+                .param("content", request.getContent())
+                .param("startDate", request.getStartDate().toString())
+                .param("endDate", request.getEndDate().toString())
+                .param("assigneeId", request.getAssigneeId().toString()))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(status().isCreated())
+            .andExpect(header().exists("Location"))
+            .andExpect(header().string("Location", "/api/projects/" + key + "/issues/1"))
+            .andExpect(jsonPath("$.code").value("COMMON_201"));
+
+
+    }
+
+}
