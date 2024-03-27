33f0d9d514d9e25e040f85ade91545b0f50c2b20
minsang-alt <mazinger97@naver.com>
2024-03-27 12:42:20 +0900

    feat(IssueController): 이슈 조회 메서드 구현

--- src/main/java/dynamicquad/agilehub/issue/controller/IssueController.java
+++ src/main/java/dynamicquad/agilehub/issue/controller/IssueController.java
@@ -2,7 +2,9 @@ package dynamicquad.agilehub.issue.controller;
 
 import dynamicquad.agilehub.global.header.CommonResponse;
 import dynamicquad.agilehub.global.header.status.SuccessStatus;
+import dynamicquad.agilehub.issue.controller.IssueResponse.IssueReadResponseDto;
 import dynamicquad.agilehub.issue.controller.request.IssueRequest.IssueCreateRequest;
+import dynamicquad.agilehub.issue.service.IssueQueryService;
 import dynamicquad.agilehub.issue.service.IssueService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -16,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IssueController {
 
     private final IssueService issueService;
+    private final IssueQueryService issueQueryService;
 
     @PostMapping(value = "/api/projects/{key}/issues", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "이슈 생성", description = "프로젝트의 이슈를 생성합니다.",
@@ -42,5 +46,13 @@ public class IssueController {
             .body(CommonResponse.of(SuccessStatus.CREATED, issueId));
     }
 
+    // 이슈 조회
+    @GetMapping(value = "/api/projects/{key}/issues/{issueId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "이슈 조회", description = "프로젝트의 이슈를 조회합니다.")
+    public CommonResponse<?> getProjectIssue(@PathVariable("key") String key, @PathVariable("issueId") Long issueId) {
+        log.info("getProjectIssue key: {}, issueId: {}", key, issueId);
+        IssueReadResponseDto issue = issueQueryService.getIssue(key, issueId);
 
+        return CommonResponse.of(SuccessStatus.OK, issue);
+    }
 }
