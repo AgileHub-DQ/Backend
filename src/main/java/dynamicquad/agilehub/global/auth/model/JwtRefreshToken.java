b12b97130d3c84c4ac2f466bef18b8511aa8dd86
enble <enble_777@naver.com>
2024-03-28 00:43:51 +0900

    feat: JwtRefreshToken 엔티티 및 repository 추가

---
+++ src/main/java/dynamicquad/agilehub/global/auth/model/JwtRefreshToken.java
@@ -0,0 +1,30 @@
+package dynamicquad.agilehub.global.auth.model;
+
+import jakarta.persistence.Id;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+/**
+ * JwtRefreshToken 갱신 : 14일
+ */
+@Getter
+@AllArgsConstructor
+@RedisHash(value = "refresh_token", timeToLive = 60 * 60 * 24 * 14)
+public class JwtRefreshToken implements Serializable {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String accessToken;
+
+    private String refreshToken;
+
+    public void updateAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+}
---
+++ src/main/java/dynamicquad/agilehub/global/auth/repository/JwtRepositoryTokenRepository.java
@@ -0,0 +1,13 @@
+package dynamicquad.agilehub.global.auth.repository;
+
+import dynamicquad.agilehub.global.auth.model.JwtRefreshToken;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JwtRepositoryTokenRepository extends CrudRepository<JwtRefreshToken, String> {
+
+    Optional<JwtRefreshToken> findByAccessToken(String accessToken);
+
+}
