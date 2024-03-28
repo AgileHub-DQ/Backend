14763d0eefb7fec0612c900c2d9481a4b26da961
Enble <enble_777@naver.com>
2024-03-28 17:59:20 +0900

    feat: SocialLoginQueryService의 메서드를 콜하는 MemberQueryService 구현

---
+++ src/main/java/dynamicquad/agilehub/member/service/MemberQueryService.java
@@ -0,0 +1,24 @@
+package dynamicquad.agilehub.member.service;
+
+import dynamicquad.agilehub.global.exception.GeneralException;
+import dynamicquad.agilehub.global.header.status.ErrorStatus;
+import dynamicquad.agilehub.member.domain.Member;
+import dynamicquad.agilehub.member.domain.SocialLogin;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberQueryService {
+
+    private final SocialLoginQueryService socialLoginQueryService;
+
+    public Member findBySocialProviderAndDistinctCode(String provider, String distinctCode) {
+        return socialLoginQueryService.findByProviderAndDistinctCode(provider, distinctCode)
+                .map(SocialLogin::getMember)
+                .orElseThrow(() -> new GeneralException(ErrorStatus.MEMBER_NOT_FOUND));
+    }
+
+}
