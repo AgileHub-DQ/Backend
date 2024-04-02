546fbf83dedea87da52bac333025f2148724bb62
Enble <enble_777@naver.com>
2024-04-02 21:16:40 +0900

    feat: Jwt 인증 filter 구현

--- src/main/java/dynamicquad/agilehub/global/filter/JwtAuthFilter.java
+++ src/main/java/dynamicquad/agilehub/global/filter/JwtAuthFilter.java
@@ -1,34 +1,55 @@
 package dynamicquad.agilehub.global.filter;
 
+import dynamicquad.agilehub.global.auth.model.SecurityMember;
 import dynamicquad.agilehub.global.auth.util.JwtUtil;
-import dynamicquad.agilehub.member.service.MemberService;
+import dynamicquad.agilehub.global.exception.JwtException;
+import dynamicquad.agilehub.global.header.status.ErrorStatus;
+import dynamicquad.agilehub.member.domain.Member;
+import dynamicquad.agilehub.member.service.MemberQueryService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
-    private final MemberService memberService;
+    private final MemberQueryService memberQueryService;
 
-    /**
+    /*
      * access token 유효 -> authentication 저장
-     * access token 만료
-     *  - refresh token 유효 -> access token 재발급, authentication 저장
+     * access token 만료 -> 401 Unauthorized 예외 발생
      */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        jwtUtil.extractAccessToken(request)
+        String token = jwtUtil.extractAccessToken(request)
+                .filter(jwtUtil::verifyToken)
+                .orElseThrow(() -> new JwtException(ErrorStatus.INVALID_ACCESS_TOKEN.getMessage()));
+
+        saveAuthentication(token);
+        filterChain.doFilter(request, response);
     }
 
     private void saveAuthentication(String accessToken) {
+        String provider = jwtUtil.extractProvider(accessToken);
+        String distinctId = jwtUtil.extractDistinctId(accessToken);
 
+        Member findMember = memberQueryService.findBySocialProviderAndDistinctId(provider, distinctId);
+        SecurityMember securityMember = new SecurityMember(findMember);
+
+        SecurityContextHolder.getContext().setAuthentication(getAuthentication(securityMember));
+    }
+
+    private Authentication getAuthentication(SecurityMember securityMember) {
+        return new UsernamePasswordAuthenticationToken(securityMember, null, securityMember.getAuthorities());
     }
 
     @Override
