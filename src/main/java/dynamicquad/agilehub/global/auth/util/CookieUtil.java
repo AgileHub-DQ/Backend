3560b2b435c084f5d63528019a770ad06890c261
Enble <enble_777@naver.com>
2024-04-05 16:56:26 +0900

    feat: CookieUtil 구현

--- src/main/java/dynamicquad/agilehub/global/auth/util/CookieUtil.java
+++ src/main/java/dynamicquad/agilehub/global/auth/util/CookieUtil.java
@@ -2,36 +2,59 @@ package dynamicquad.agilehub.global.auth.util;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Value;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Base64;
+import java.util.Optional;
 import org.springframework.stereotype.Component;
+import org.springframework.util.SerializationUtils;
 
 @Component
 public class CookieUtil {
 
-    @Value("${jwt.refresh.expiration}")
-    private long refreshTokenValidationSeconds;
-
-    public Cookie generateCookie(String name, String value) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        cookie.setMaxAge((int) refreshTokenValidationSeconds);
-        return cookie;
-    }
-
-    public Cookie getCookie(HttpServletRequest request, String name) {
+    public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
         Cookie[] cookies = request.getCookies();
-        if (cookies == null) {
-            return null;
-        }
 
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals(name)) {
-                return cookie;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(name)) {
+                    return Optional.of(cookie);
+                }
             }
         }
 
-        return null;
+        return Optional.empty();
+    }
+
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+
+    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(name)) {
+                    cookie.setValue("");
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                }
+            }
+        }
+    }
+
+
+    public static String serialize(Object object) {
+        return Base64.getUrlEncoder().encodeToString(SerializationUtils.serialize(object));
+    }
+
+    public static <T> T deserialize(Cookie cookie, Class<T> clazz) {
+        return clazz.cast(SerializationUtils.deserialize(Base64.getUrlDecoder().decode(cookie.getValue())));
     }
 
 }
