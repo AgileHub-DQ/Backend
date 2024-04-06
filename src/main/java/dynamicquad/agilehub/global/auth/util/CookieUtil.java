2bee635f20b502b914bfdb5a37c803a7df70d74f
enble <enble_777@naver.com>
2024-04-06 19:08:53 +0900

    refactor: CookieUtil을 non-static으로 변경

--- src/main/java/dynamicquad/agilehub/global/auth/util/CookieUtil.java
+++ src/main/java/dynamicquad/agilehub/global/auth/util/CookieUtil.java
@@ -11,7 +11,7 @@ import org.springframework.util.SerializationUtils;
 @Component
 public class CookieUtil {
 
-    public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+    public Optional<Cookie> getCookie(HttpServletRequest request, String name) {
         Cookie[] cookies = request.getCookies();
 
         if (cookies != null) {
@@ -25,7 +25,7 @@ public class CookieUtil {
         return Optional.empty();
     }
 
-    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+    public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
 
         cookie.setPath("/");
@@ -34,7 +34,7 @@ public class CookieUtil {
         response.addCookie(cookie);
     }
 
-    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+    public void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
@@ -49,11 +49,11 @@ public class CookieUtil {
     }
 
 
-    public static String serialize(Object object) {
+    public String serialize(Object object) {
         return Base64.getUrlEncoder().encodeToString(SerializationUtils.serialize(object));
     }
 
-    public static <T> T deserialize(Cookie cookie, Class<T> clazz) {
+    public <T> T deserialize(Cookie cookie, Class<T> clazz) {
         return clazz.cast(SerializationUtils.deserialize(Base64.getUrlDecoder().decode(cookie.getValue())));
     }
 
