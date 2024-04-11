8b0587ff8c09300d6373cf856dbfafb7ef7c6fd1
enble <enble_777@naver.com>
2024-04-11 10:38:37 +0900

    style: WIP

--- src/main/java/dynamicquad/agilehub/global/auth/model/SecurityMember.java
+++ src/main/java/dynamicquad/agilehub/global/auth/model/SecurityMember.java
@@ -7,10 +7,11 @@ import java.util.List;
 import java.util.Map;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @Getter
-public class SecurityMember implements OAuth2User {
+public class SecurityMember implements  OAuth2User,UserDetails {
 
     private final Member member;
     private OAuth2UserInfo userInfo;
@@ -45,6 +46,36 @@ public class SecurityMember implements OAuth2User {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(() -> "ROLE_USER");
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return userInfo.getNickname();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
