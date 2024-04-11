4a47cbe02d279a5940a87e3e8d8a364d130f24cf
Enble <enble_777@naver.com>
2024-04-11 16:58:38 +0900

    refactor: Redis Hash value 변경 및 TTL 필드 추가

--- src/main/java/dynamicquad/agilehub/global/auth/model/JwtRefreshToken.java
+++ src/main/java/dynamicquad/agilehub/global/auth/model/JwtRefreshToken.java
@@ -4,13 +4,14 @@ import jakarta.persistence.Id;
 import java.io.Serializable;
 import lombok.Getter;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
 /**
  * JwtRefreshToken 갱신 : 14일
  */
 @Getter
-@RedisHash(value = "refresh_token", timeToLive = 60 * 60 * 24 * 14)
+@RedisHash(value = "jwt_token", timeToLive = 60 * 60 * 24 * 14)
 public class JwtRefreshToken implements Serializable {
 
     @Id
@@ -21,9 +22,17 @@ public class JwtRefreshToken implements Serializable {
 
     private String refreshToken;
 
+    @TimeToLive
+    private Long expiration;
+
     public JwtRefreshToken(String accessToken, String refreshToken) {
+        new JwtRefreshToken(accessToken, refreshToken, (long) (60 * 60));
+    }
+
+    public JwtRefreshToken(String accessToken, String refreshToken, Long expiration) {
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
+        this.expiration = expiration;
     }
 
     public void updateAccessToken(String accessToken) {
