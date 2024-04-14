4fbc7c17b9df0be6a432f08436ba9593be477c30
enble <enble_777@naver.com>
2024-04-14 21:05:00 +0900

    refactor: repository 어노테이션 추가

--- src/main/java/dynamicquad/agilehub/global/auth/repository/RefreshTokenRedisRepository.java
+++ src/main/java/dynamicquad/agilehub/global/auth/repository/RefreshTokenRedisRepository.java
@@ -3,7 +3,9 @@ package dynamicquad.agilehub.global.auth.repository;
 import dynamicquad.agilehub.global.auth.model.JwtRefreshToken;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RefreshTokenRedisRepository extends CrudRepository<JwtRefreshToken, String> {
 
     Optional<JwtRefreshToken> findByAccessToken(String accessToken);
