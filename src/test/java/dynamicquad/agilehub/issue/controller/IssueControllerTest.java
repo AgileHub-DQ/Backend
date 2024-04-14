41d27a6537c924b7d508a3e84cc6b3416502a42b
enble <enble_777@naver.com>
2024-04-15 00:58:28 +0900

    fix!: [NOT FIXED YET] DI 못받는 문제 해결중

--- src/test/java/dynamicquad/agilehub/issue/controller/IssueControllerTest.java
+++ src/test/java/dynamicquad/agilehub/issue/controller/IssueControllerTest.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import dynamicquad.agilehub.WithMockCustomUser;
 import dynamicquad.agilehub.issue.controller.request.IssueRequest.IssueCreateRequest;
 import dynamicquad.agilehub.issue.controller.request.IssueType;
 import dynamicquad.agilehub.issue.domain.IssueStatus;
@@ -36,37 +37,38 @@ class IssueControllerTest {
     private IssueQueryService issueQueryService;
 
     @Test
+    @WithMockCustomUser
     void 정상적인_이슈를_생성하면_해당_이슈를_가진_URL을_반환한다() throws Exception {
 
         //given
         IssueCreateRequest request = IssueCreateRequest.builder()
-            .title("이슈제목")
-            .type(IssueType.EPIC)
-            .status(IssueStatus.DO)
-            .content("이슈내용")
-            .files(null)
-            .startDate(LocalDate.of(2021, 1, 1))
-            .endDate(LocalDate.of(2021, 1, 2))
-            .assigneeId(1L)
-            .build();
+                .title("이슈제목")
+                .type(IssueType.EPIC)
+                .status(IssueStatus.DO)
+                .content("이슈내용")
+                .files(null)
+                .startDate(LocalDate.of(2021, 1, 1))
+                .endDate(LocalDate.of(2021, 1, 2))
+                .assigneeId(1L)
+                .build();
 
         String key = "project";
         when(issueService.createIssue(key, request)).thenReturn(1L);
 
         mockMvc.perform(post("/api/projects/" + key + "/issues")
-                .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
-                .param("title", request.getTitle())
-                .param("type", request.getType().name())
-                .param("status", request.getStatus().name())
-                .param("content", request.getContent())
-                .param("startDate", request.getStartDate().toString())
-                .param("endDate", request.getEndDate().toString())
-                .param("assigneeId", request.getAssigneeId().toString()))
-            .andDo(MockMvcResultHandlers.print())
-            .andExpect(status().isCreated())
-            .andExpect(header().exists("Location"))
-            .andExpect(header().string("Location", "/api/projects/" + key + "/issues/1"))
-            .andExpect(jsonPath("$.code").value("COMMON_201"));
+                        .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                        .param("title", request.getTitle())
+                        .param("type", request.getType().name())
+                        .param("status", request.getStatus().name())
+                        .param("content", request.getContent())
+                        .param("startDate", request.getStartDate().toString())
+                        .param("endDate", request.getEndDate().toString())
+                        .param("assigneeId", request.getAssigneeId().toString()))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(header().string("Location", "/api/projects/" + key + "/issues/1"))
+                .andExpect(jsonPath("$.code").value("COMMON_201"));
 
 
     }
