563c832e78bc7e8971a5c78ccfe432586d68e3ab
minsang-alt <mazinger97@naver.com>
2024-04-26 16:46:07 +0900

    refactor: 사용되지 않은 쿼리 제거

--- src/main/java/dynamicquad/agilehub/issue/domain/epic/EpicRepository.java
+++ src/main/java/dynamicquad/agilehub/issue/domain/epic/EpicRepository.java
@@ -5,7 +5,6 @@ import dynamicquad.agilehub.project.domain.Project;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,8 +12,6 @@ public interface EpicRepository extends JpaRepository<Epic, Long>, EpicStaticsRe
 
     List<Epic> findByProject(Project project);
 
-    @Query("SELECT e FROM Epic e WHERE e.project = :project")
-    List<Epic> findEpicsByProject(@Param("project") Project project);
 
     @Query(value = "SELECT e.issue_id AS epicId, COUNT(DISTINCT s.issue_id) AS storiesCount, "
         + "SUM(CASE WHEN i.status = 'DO' THEN 1 ELSE 0 END) AS statusDo, "
