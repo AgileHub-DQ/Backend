83746f7ba1353a873494d75162307b7388617afe
Min Sang <minseusang@gmail.com>
2024-04-27 22:42:12 +0900

    refactor: config 수정 (#70)

--- src/main/java/dynamicquad/agilehub/global/config/SwaggerConfig.java
+++ src/main/java/dynamicquad/agilehub/global/config/SwaggerConfig.java
@@ -6,25 +6,34 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(
-                title = "AgileHub API 명세서",
-                version = "1.0"
-        ),
-        security = {
-                @SecurityRequirement(name = "bearerAuth")
-        }
+    info = @Info(
+        title = "AgileHub API 명세서",
+        version = "1.0"
+    ),
+    security = {
+        @SecurityRequirement(name = "bearerAuth")
+    }
 )
 @SecurityScheme(
-        name = "bearerAuth",
-        description = "JWT authentication",
-        scheme = "bearer",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        in = SecuritySchemeIn.HEADER
+    name = "bearerAuth",
+    description = "JWT authentication",
+    scheme = "bearer",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    in = SecuritySchemeIn.HEADER
 )
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .addServersItem(new Server().url("/"));
+    }
 }
