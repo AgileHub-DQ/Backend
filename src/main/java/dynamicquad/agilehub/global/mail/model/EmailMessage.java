77281a92d591f90e42133044a37969b38fcdad78
Enble <enble_777@naver.com>
2024-04-30 16:36:23 +0900

    refactor: 초대코드를 넣을 수 있도록 EmailService 수정

--- src/main/java/dynamicquad/agilehub/global/mail/model/EmailMessage.java
+++ src/main/java/dynamicquad/agilehub/global/mail/model/EmailMessage.java
@@ -11,11 +11,21 @@ public class EmailMessage {
     private String to;          // 수신자
     private String subject;     // 제목
     private String message;     // 내용
-    
+
+    private String inviteCode;  // 초대코드
+
     @Builder
     public EmailMessage(String to, String subject, String message) {
         this.to = to;
         this.subject = subject;
         this.message = message;
+    }
+
+    @Builder
+    public EmailMessage(String to, String subject, String message, String inviteCode) {
+        this.to = to;
+        this.subject = subject;
+        this.message = message;
+        this.inviteCode = inviteCode;
     }
 }
--- src/main/java/dynamicquad/agilehub/global/mail/service/EmailService.java
+++ src/main/java/dynamicquad/agilehub/global/mail/service/EmailService.java
@@ -9,25 +9,38 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
 
-    public void sendMail(EmailMessage emailMessage) {
+    public void sendMail(EmailMessage emailMessage, String type) {
         MimeMessage mimeMailMessage = mailSender.createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(mimeMailMessage, false, "UTF-8");
-            helper.setFrom("noreply-agilehubdq@gmail.com");
             helper.setTo(emailMessage.getTo());
             helper.setSubject(emailMessage.getSubject());
-            helper.setText(emailMessage.getMessage());
+            if (StringUtils.hasText(type)) {
+                helper.setText(setContext(emailMessage, type), true);
+            } else {
+                helper.setText(emailMessage.getMessage());
+            }
             mailSender.send(mimeMailMessage);
         } catch (MessagingException me) {
             throw new GeneralException(ErrorStatus.EMAIL_NOT_SENT);
         }
+    }
+
+    public String setContext(EmailMessage emailMessage, String type) {
+        Context context = new Context();
+        context.setVariable("inviteCode", emailMessage.getInviteCode());
+        return templateEngine.process(type, context);
     }
 
 }
