6847286846bdf7b1742fafe385107be8f26c5a9c
Enble <enble_777@naver.com>
2024-05-01 11:06:18 +0900

    feat: findProjectById() 구현

--- src/main/java/dynamicquad/agilehub/project/service/ProjectQueryService.java
+++ src/main/java/dynamicquad/agilehub/project/service/ProjectQueryService.java
@@ -1,8 +1,11 @@
 package dynamicquad.agilehub.project.service;
 
+import dynamicquad.agilehub.global.exception.GeneralException;
+import dynamicquad.agilehub.global.header.status.ErrorStatus;
 import dynamicquad.agilehub.member.service.MemberService;
 import dynamicquad.agilehub.project.controller.response.ProjectResponse;
 import dynamicquad.agilehub.project.domain.Project;
+import dynamicquad.agilehub.project.domain.ProjectRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,13 +22,18 @@ public class ProjectQueryService {
     private final MemberService memberService;
     private final MemberProjectService memberProjectService;
 
-    public List<ProjectResponse> getProjects(Long memberId) {
+    private final ProjectRepository projectRepository;
 
+    public List<ProjectResponse> getProjects(Long memberId) {
         memberService.validateMemberExist(memberId);
         List<Project> projects = memberProjectService.findProjects(memberId);
 
         return projects.stream().map(ProjectResponse::fromEntity).toList();
     }
 
+    public ProjectResponse findProjectById(Long projectId) {
+        return projectRepository.findById(projectId).map(ProjectResponse::fromEntity)
+                .orElseThrow(() -> new GeneralException(ErrorStatus.PROJECT_NOT_FOUND));
+    }
 
 }
