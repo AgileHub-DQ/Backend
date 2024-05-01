8e7f5331677e78c9a224247b4c71d3174c6eb636
Enble <enble_777@naver.com>
2024-05-01 12:35:11 +0900

    fix: @SuperBuilder를 통한 Builder 문제 해결

--- src/main/java/dynamicquad/agilehub/global/mail/model/EmailInfo.java
+++ src/main/java/dynamicquad/agilehub/global/mail/model/EmailInfo.java
@@ -1,16 +1,16 @@
 package dynamicquad.agilehub.global.mail.model;
 
-import lombok.Builder;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
+@SuperBuilder
 public class EmailInfo {
 
     private final String to;          // 수신자
     private final String subject;     // 제목
     private final String message;     // 내용
 
-    @Builder
     public EmailInfo(String to, String subject, String message) {
         this.to = to;
         this.subject = subject;
