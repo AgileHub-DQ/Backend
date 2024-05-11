634e667f6f248ac8e479d408e71c3dc06b2108a7
minsang-alt <mazinger97@naver.com>
2024-05-11 16:00:30 +0900

    refactor: 쓰이지 않은 querydsl로 구현된 클래스 제거

--- src/main/java/dynamicquad/agilehub/issue/domain/epic/EpicStaticsRepository.java
+++
@@ -1,8 +0,0 @@
-package dynamicquad.agilehub.issue.domain.epic;
-
-import dynamicquad.agilehub.issue.controller.response.EpicResponse.EpicStatisticDto;
-import java.util.List;
-
-public interface EpicStaticsRepository {
-    List<EpicStatisticDto> getEpicStatics(Long projectId);
-}
