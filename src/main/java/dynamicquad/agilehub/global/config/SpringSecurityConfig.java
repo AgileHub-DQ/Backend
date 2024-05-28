1c8b0fd6e5d0adbfe162906d6df64cb4d6bc87f9
Enble <enble_777@naver.com>
2024-05-28 16:18:50 +0900

    feat: CustomAuthorizationRequestRepository 반영

--- src/main/java/dynamicquad/agilehub/global/config/SpringSecurityConfig.java
+++ src/main/java/dynamicquad/agilehub/global/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package dynamicquad.agilehub.global.config;
 import dynamicquad.agilehub.global.auth.filter.JwtAuthFilter;
 import dynamicquad.agilehub.global.auth.filter.JwtExceptionFilter;
 import dynamicquad.agilehub.global.auth.filter.OAuth2SuccessHandler;
+import dynamicquad.agilehub.global.auth.repository.CustomAuthorizationRequestRepository;
 import dynamicquad.agilehub.global.auth.service.CustomOAuth2UserService;
 import java.util.Collections;
 import java.util.List;
@@ -34,38 +35,41 @@ public class SpringSecurityConfig {
 
         // csrf disable 처리 : 추후 설정 변경 필요
         http
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .cors(cors -> cors.configurationSource(request -> {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOriginPatterns(Collections.singletonList("*"));
-                config.setAllowedMethods(Collections.singletonList("*"));
-                config.setAllowedHeaders(Collections.singletonList("*"));
-                config.setAllowCredentials(true);
-                config.setExposedHeaders(List.of("Authorization"));
-                config.setMaxAge(3600L);
-                return config;
-            }))
-            .csrf(AbstractHttpConfigurer::disable)
-            .formLogin(AbstractHttpConfigurer::disable)
-            .httpBasic(AbstractHttpConfigurer::disable)
-            .logout(AbstractHttpConfigurer::disable)
-            // requestMatchers 설정
-            .authorizeHttpRequests(requests -> requests
-                .requestMatchers("/oauth2/**", "/auth/success/**", "*/api-docs/**", "/swagger-ui/**", "/actuator/**",
-                    "/favicon.ico")
-                .permitAll()
-                .anyRequest().authenticated()
-            )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOriginPatterns(Collections.singletonList("*"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setExposedHeaders(List.of("Authorization"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }))
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                // requestMatchers 설정
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/oauth2/**", "/auth/success/**", "*/api-docs/**", "/swagger-ui/**",
+                                "/actuator/**",
+                                "/favicon.ico")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
 
-            // oauth2 설정
-            .oauth2Login(customizer -> customizer
-                .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
-                .successHandler(oAuth2SuccessHandler)
-            )
+                // oauth2 설정
+                .oauth2Login(customizer -> customizer
+                        .authorizationEndpoint(authorization -> authorization
+                                .authorizationRequestRepository(new CustomAuthorizationRequestRepository()))
+                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+                        .successHandler(oAuth2SuccessHandler)
+                )
 
-            // jwt 설정
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(new JwtExceptionFilter(), JwtAuthFilter.class);
+                // jwt 설정
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtExceptionFilter(), JwtAuthFilter.class);
 
         return http.build();
     }
