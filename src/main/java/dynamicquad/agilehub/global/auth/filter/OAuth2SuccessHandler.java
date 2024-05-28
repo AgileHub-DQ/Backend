8781fce310e74ad885f11eda3adbf1e1ce4518ed
Enble <68385400+Enble@users.noreply.github.com>
2024-05-28 17:50:57 +0900

    [#116] 소셜 로그인에서 프론트가 리다이렉트 URL 설정할 수 있도록 변경 (#122)
    * feat: CookieUtil 구현
    
    * feat: CustomAuthorizationRequestRepository 구현
    
    * feat: CustomAuthorizationRequestRepository 반영
    
    * refactor: OAuth2SuccessHandler에서 redirect url을 수동 설정하도록 변경
    
    * refactor: 쿠키 유지 시간 30분으로 설정

--- src/main/java/dynamicquad/agilehub/global/auth/filter/OAuth2SuccessHandler.java
+++ src/main/java/dynamicquad/agilehub/global/auth/filter/OAuth2SuccessHandler.java
@@ -1,9 +1,13 @@
 package dynamicquad.agilehub.global.auth.filter;
 
+import static dynamicquad.agilehub.global.auth.repository.CustomAuthorizationRequestRepository.REDIRECT_URL_PARAM_COOKIE_NAME;
+
 import dynamicquad.agilehub.global.auth.model.GeneratedToken;
 import dynamicquad.agilehub.global.auth.model.JwtRefreshToken;
 import dynamicquad.agilehub.global.auth.model.SecurityMember;
+import dynamicquad.agilehub.global.auth.repository.CustomAuthorizationRequestRepository;
 import dynamicquad.agilehub.global.auth.service.RefreshTokenRedisService;
+import dynamicquad.agilehub.global.auth.util.CookieUtil;
 import dynamicquad.agilehub.global.auth.util.JwtUtil;
 import dynamicquad.agilehub.global.exception.GeneralException;
 import dynamicquad.agilehub.global.header.status.ErrorStatus;
@@ -11,20 +15,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import java.io.IOException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    private static final String REDIRECT_URL = "/";
-    private static final String CLIENT_DOMAIN = "https://www.agilehub.store";
+    private final CustomAuthorizationRequestRepository customAuthorizationRequestRepository;
 
     private final RefreshTokenRedisService redisService;
 
@@ -34,13 +38,31 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     @Transactional
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
+        String targetUrl = determineTargetUrl(request, response, authentication);
         GeneratedToken generatedToken = generateMemberToken(authentication);
 
-        String redirectUrl = UriComponentsBuilder.fromUriString(REDIRECT_URL)
-            .queryParam("accessToken", generatedToken.getAccessToken())
-            .build().toUriString();
+        String redirectUrl = getRedirectUrl(targetUrl, generatedToken);
+        getRedirectStrategy().sendRedirect(request, response, redirectUrl);
+    }
 
-        response.sendRedirect(CLIENT_DOMAIN + redirectUrl);
+    @Override
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) {
+        Optional<String> redirectUri = CookieUtil.getCookie(request, REDIRECT_URL_PARAM_COOKIE_NAME)
+                .map(cookie -> cookie.getValue());
+
+        clearAuthenticationAttributes(request, response);
+        return redirectUri.orElse(getDefaultTargetUrl());
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        customAuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+    }
+
+    private String getRedirectUrl(String targetUrl, GeneratedToken generatedToken) {
+        return UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("accessToken", generatedToken.getAccessToken())
+                .build().toUriString();
     }
 
     private GeneratedToken generateMemberToken(Authentication authentication) {
@@ -49,7 +71,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         String distinctId = principal.getId();
         String name = principal.getName();
         String role = principal.getAuthorities().stream().findFirst()
-            .orElseThrow(() -> new GeneralException(ErrorStatus.MEMBER_ROLE_NOT_EXIST)).getAuthority();
+                .orElseThrow(() -> new GeneralException(ErrorStatus.MEMBER_ROLE_NOT_EXIST)).getAuthority();
 
         GeneratedToken token = jwtUtil.generateToken(name, role, provider, distinctId);
 
---
+++ src/main/java/dynamicquad/agilehub/global/auth/repository/CustomAuthorizationRequestRepository.java
@@ -0,0 +1,55 @@
+package dynamicquad.agilehub.global.auth.repository;
+
+import dynamicquad.agilehub.global.auth.util.CookieUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CustomAuthorizationRequestRepository implements
+        AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
+
+    public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+    public static final String REDIRECT_URL_PARAM_COOKIE_NAME = "redirect_url";
+
+    private static final int cookieExpireSeconds = 30 * 60; // 30 minutes
+
+    @Override
+    public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
+        return CookieUtil.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME)
+                .map(cookie -> CookieUtil.deserialize(cookie, OAuth2AuthorizationRequest.class))
+                .orElse(null);
+    }
+
+    @Override
+    public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request,
+                                         HttpServletResponse response) {
+        if (authorizationRequest == null) {
+            CookieUtil.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+            CookieUtil.deleteCookie(request, response, REDIRECT_URL_PARAM_COOKIE_NAME);
+            return;
+        }
+
+        CookieUtil.addCookie(response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME,
+                CookieUtil.serialize(authorizationRequest), cookieExpireSeconds);
+        String redirectUriAfterLogin = request.getParameter(REDIRECT_URL_PARAM_COOKIE_NAME);
+
+        if (redirectUriAfterLogin != null && !redirectUriAfterLogin.isBlank()) {
+            CookieUtil.addCookie(response, REDIRECT_URL_PARAM_COOKIE_NAME, redirectUriAfterLogin, cookieExpireSeconds);
+        }
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request,
+                                                                 HttpServletResponse response) {
+        return this.loadAuthorizationRequest(request);
+    }
+
+    public void removeAuthorizationRequestCookies(HttpServletRequest request, HttpServletResponse response) {
+        CookieUtil.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+        CookieUtil.deleteCookie(request, response, REDIRECT_URL_PARAM_COOKIE_NAME);
+    }
+
+}
