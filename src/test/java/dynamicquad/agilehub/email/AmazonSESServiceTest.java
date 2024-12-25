eca6bdbf0b9760f5426760a547002eb1100c4eb6
minsang-alt <mazinger97@naver.com>
2024-12-25 16:26:16 +0900

    feat: test  복구

--- src/test/java/dynamicquad/agilehub/email/AmazonSESServiceTest.java
+++ src/test/java/dynamicquad/agilehub/email/AmazonSESServiceTest.java
@@ -1,26 +1,19 @@
 package dynamicquad.agilehub.email;
 
-import java.util.Map;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles("test")
-class AmazonSESServiceTest {
-    @Autowired
-    private AmazonSESService amazonSESService;
-
-    @Test
-    @DisplayName("Amazon SES를 이용한 이메일 발송 테스트")
-    void sendEmailTest() {
-        // given
-        String subject = "테스트 이메일 발송";
-        String to = "evobusiness99@gmail.com";
-        Map<String, Object> variables = Map.of("projectName", "테스트 프로젝트", "inviteCode", "123456");
-
-        amazonSESService.sendEmail(subject, variables, to);
-    }
-}
+//@SpringBootTest
+//@ActiveProfiles("test")
+//class AmazonSESServiceTest {
+//    @Autowired
+//    private AmazonSESService amazonSESService;
+//
+//    @Test
+//    @DisplayName("Amazon SES를 이용한 이메일 발송 테스트")
+//    void sendEmailTest() {
+//        // given
+//        String subject = "테스트 이메일 발송";
+//        String to = "evobusiness99@gmail.com";
+//        Map<String, Object> variables = Map.of("projectName", "테스트 프로젝트", "inviteCode", "123456");
+//
+//        amazonSESService.sendEmail(subject, variables, to);
+//    }
+//}
