406cf04f74dd7010fd9eabbb7af5ae909a83ec21
minsang-alt <mazinger97@naver.com>
2025-02-13 14:50:56 +0900

    refactor: gmail smtp retry 재시도 오류 수정

--- src/main/java/dynamicquad/agilehub/global/mail/service/EmailService.java
+++ src/main/java/dynamicquad/agilehub/global/mail/service/EmailService.java
@@ -3,18 +3,17 @@ package dynamicquad.agilehub.global.mail.service;
 import dynamicquad.agilehub.global.exception.GeneralException;
 import dynamicquad.agilehub.global.header.status.ErrorStatus;
 import dynamicquad.agilehub.global.mail.model.EmailInfo;
-import dynamicquad.agilehub.issue.aspect.Retry;
 import dynamicquad.agilehub.project.model.InviteEmailInfo;
 import dynamicquad.agilehub.report.SummaryEmailInfo;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-
 import java.util.concurrent.CompletableFuture;
-
-import org.springframework.mail.MailSendException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -23,22 +22,21 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
 
     private final JavaMailSender mailSender;
     private final SpringTemplateEngine templateEngine;
 
     @Async("emailExecutor")
-    @Retry(maxRetries = 3, // 최대 3번 시도
-            retryFor = { MessagingException.class, MailSendException.class }, // 이메일 관련 예외만 재시도
-            delay = 1000 // 1초 대기 후 재시도
-    )
+    @Retryable(retryFor = {GeneralException.class}, maxAttempts = 3, backoff = @Backoff(delay = 500))
     public CompletableFuture<Void> sendMail(EmailInfo emailInfo, String type) {
         return CompletableFuture.runAsync(() -> {
             try {
                 MimeMessage message = createMimeMessage(emailInfo, type);
                 mailSender.send(message);
-            } catch (MessagingException me) {
+            } catch (Exception e) {
+                log.error("Gmail SMTP 이메일 발송 실패: {}", e.getMessage());
                 throw new GeneralException(ErrorStatus.EMAIL_NOT_SENT);
             }
         });
@@ -57,8 +55,8 @@ public class EmailService {
 
     private String getEmailContent(EmailInfo emailInfo, String type) {
         return StringUtils.hasText(type)
-                ? setContext(emailInfo, type)
-                : emailInfo.getMessage();
+            ? setContext(emailInfo, type)
+            : emailInfo.getMessage();
     }
 
     private String setContext(EmailInfo emailInfo, String type) {
@@ -66,7 +64,8 @@ public class EmailService {
         if (type.equals("invite") && emailInfo instanceof InviteEmailInfo inviteEmailMessage) {
             context.setVariable("projectName", inviteEmailMessage.getProjectName());
             context.setVariable("inviteCode", inviteEmailMessage.getInviteCode());
-        } else if (type.equals("reportSummary") && emailInfo instanceof SummaryEmailInfo summaryEmailMessage) {
+        }
+        else if (type.equals("reportSummary") && emailInfo instanceof SummaryEmailInfo summaryEmailMessage) {
             context.setVariable("projectName", summaryEmailMessage.getProjectName());
             context.setVariable("report", summaryEmailMessage.getReport());
         }
